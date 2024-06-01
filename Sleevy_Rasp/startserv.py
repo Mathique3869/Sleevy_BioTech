d9f9910e52be56a5073eb7f26befb80f9d496cc9
Mathique3869 <mathis.renoux@gmail.com>
2024-06-01 17:12:30 +0200

    automatisation au demarage

---
+++ Sleevy_Rasp/startserv.py
@@ -0,0 +1,15 @@
+import RecPPGEMG
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Où était le Gondor lorsque le Westfold est tombé ?'
+
+@app.route('/startrec')
+def startrec():
+    RecPPGEMG.all()
+    return 'prout'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
